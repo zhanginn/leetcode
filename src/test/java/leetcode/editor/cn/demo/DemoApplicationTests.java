3721ebe3457dac5033e4222259c8c0e670ae5886
chy <zhanginn@gmail.com>
2021-05-06 10:04:18 +0800

    init

---
+++ src/test/java/leetcode/editor/cn/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package leetcode.editor.cn.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
