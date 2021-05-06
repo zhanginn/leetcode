68d825afc2a6c249d2308d19bd0a5e6980a0f980
chy <zhanginn@gmail.com>
2021-05-06 10:21:44 +0800

    init

--- src/main/java/leetcode/editor/cn/_1TwoSum.java
+++ src/main/java/leetcode/editor/cn/_1TwoSum.java
@@ -43,18 +43,35 @@ package leetcode.editor.cn;
 // Related Topics 数组 哈希表 
 // 👍 10993 👎 0
 
-public class _1TwoSum{
+import java.util.Arrays;
+
+public class _1TwoSum {
     public static void main(String[] args) {
-    Solution solution = new _1TwoSum().new Solution();
-    
+        Solution solution = new _1TwoSum().new Solution();
+        System.out.println(Arrays.toString(solution.twoSum(new int[]{2, 7, 11, 15}, 9)));
+        System.out.println(Arrays.toString(solution.twoSum(new int[]{3, 2, 4}, 6)));
+        System.out.println(Arrays.toString(solution.twoSum(new int[]{3, 3}, 6)));
     }
 
-//leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        return null;
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            if (nums == null || nums.length < 2) {
+                return null;
+            }
+            int result[] = new int[2];
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        result[0] = i;
+                        result[1] = j;
+                        return result;
+                    }
+                }
+            }
+            return result;
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
