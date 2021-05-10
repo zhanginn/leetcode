8a23d0616a91d72d154f1df14a2de27244a25869
chy <zhanginn@gmail.com>
2021-05-10 17:12:47 +0800

    init

---
+++ src/main/java/leetcode/editor/cn/_31NextPermutation.java
@@ -0,0 +1,80 @@
+package leetcode.editor.cn;
+
+//实现获取 下一个排列 的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。 
+//
+// 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。 
+//
+// 必须 原地 修改，只允许使用额外常数空间。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,2,3]
+//输出：[1,3,2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,1]
+//输出：[1,2,3]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [1,1,5]
+//输出：[1,5,1]
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：nums = [1]
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 100 
+// 0 <= nums[i] <= 100 
+// 
+// Related Topics 数组 
+// 👍 1105 👎 0
+
+public class _31NextPermutation {
+    public static void main(String[] args) {
+        Solution solution = new _31NextPermutation().new Solution();
+        solution.nextPermutation(new int[]{3, 2, 1});
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public void nextPermutation(int[] nums) {
+            int count = 0;
+            for (int i = nums.length - 1; i > 1; i--) {
+                if (nums[i] > nums[i - 1]) {
+                    int temp = nums[i];
+                    nums[i] = nums[i - 1];
+                    nums[i - 1] = temp;
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0) {
+                for (int i = 0; i < nums.length / 2; i++) {
+                    int temp = nums[i];
+                    nums[i] = nums[nums.length - i - 1];
+                    nums[nums.length - i - 1] = temp;
+                }
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
