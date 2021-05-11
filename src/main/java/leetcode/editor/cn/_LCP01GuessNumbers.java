52f43737d0992ef97cdebe12d3218ba4bc831195
c_chou <zhanginn@gmail.com>
2021-05-11 15:30:10 +0800

    init

---
+++ src/main/java/leetcode/editor/cn/_LCP01GuessNumbers.java
@@ -0,0 +1,59 @@
+//小A 和 小B 在玩猜数字。小B 每次从 1, 2, 3 中随机选择一个
+// ，小A 每次也从 1, 2, 3 中选择一个猜。他们一共进行三次这个游戏，请返回 小
+//A 猜对了几次？ 
+//
+// 输入的guess数组为 小A 每次的猜测，answer数组为 小B 每次的选择。guess和answer的长度都等于3。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：guess = [1,2,3], answer = [1,2,3]
+//输出：3
+//解释：小A 每次都猜对了。 
+//
+// 示例 2： 
+//
+// 
+//输入：guess = [2,2,3], answer = [3,2,1]
+//输出：1
+//解释：小A 只猜对了第二次。 
+//
+// 
+//
+// 限制： 
+//
+// 
+// guess 的长度 = 3 
+// answer 的长度 = 3 
+// guess 的元素取值为 {1, 2, 3} 之一。 
+// answer 的元素取值为 {1, 2, 3} 之一。 
+// 
+// 👍 124 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.Set;
+
+public class _LCP01GuessNumbers {
+    public static void main(String[] args) {
+        Solution solution = new _LCP01GuessNumbers().new Solution();
+        System.out.println(solution.game(new int[]{1, 2, 3}, new int[]{1, 2, 3}));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int game(int[] guess, int[] answer) {
+            int count = 0;
+            for (int i = 0; i < guess.length; i++) {
+                if (guess[i] == answer[i]) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
