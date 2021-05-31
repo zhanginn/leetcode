713cff31f96fd848dcd6ef0996fd5d3ee89a4dbb
c_chou <zhanginn@gmail.com>
2021-05-31 11:35:27 +0800

    init

--- src/main/java/leetcode/editor/cn/_66PlusOne.java
+++ src/main/java/leetcode/editor/cn/_66PlusOne.java
@@ -51,26 +51,16 @@ public class _66PlusOne {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int[] plusOne(int[] digits) {
-            boolean otherArr = false;
             for (int i = digits.length - 1; i >= 0; i--) {
                 if (digits[i] != 9) {
                     digits[i]++;
-                    break;
+                    return digits;
                 }
                 digits[i] = 0;
-                if (i == 0) {
-                    otherArr = true;
-                }
             }
-            if (otherArr) {
-                int[] ans = new int[digits.length + 1];
-                ans[0] = 1;
-                for (int i = 1; i < digits.length; i++) {
-                    ans[i] = digits[i - 1];
-                }
-                return ans;
-            }
-            return digits;
+            int[] ans = new int[digits.length + 1];
+            ans[0] = 1;
+            return ans;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
