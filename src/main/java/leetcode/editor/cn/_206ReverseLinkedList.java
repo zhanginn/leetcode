fc81eb3ffc5ef5660cde18db41341508b88bd06d
c_chou <zhanginn@gmail.com>
2021-06-03 16:42:18 +0800

    init

---
+++ src/main/java/leetcode/editor/cn/_206ReverseLinkedList.java
@@ -0,0 +1,95 @@
+//给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+// 
+// 
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5]
+//输出：[5,4,3,2,1]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1,2]
+//输出：[2,1]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点的数目范围是 [0, 5000] 
+// -5000 <= Node.val <= 5000 
+// 
+//
+// 
+//
+// 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？ 
+// 
+// 
+// Related Topics 链表 
+// 👍 1772 👎 0
+
+package leetcode.editor.cn;
+
+public class _206ReverseLinkedList {
+    public static void main(String[] args) {
+        Solution solution = new _206ReverseLinkedList().new Solution();
+        System.out.println(solution.reverseList(new ListNode(1, new ListNode(2, new ListNode(3)))));
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode reverseList(ListNode head) {
+            ListNode pre = null;
+            ListNode next = null;
+            while (head != null) {
+                next = head.next;
+                head.next = pre;
+                pre = head;
+                head = next;
+            }
+            return pre;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
