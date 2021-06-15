fa4995af696bc0ada507a70debbf365e46f48237
c_chou <zhanginn@gmail.com>
2021-06-15 11:16:21 +0800

    init

--- src/main/java/leetcode/editor/cn/_0sort.java
+++ src/main/java/leetcode/editor/cn/_0sort.java
@@ -44,36 +44,30 @@ public class _0sort {
     }
 
     public static void swap(int[] arr, int i, int j) {
+        if (i == j) {
+            return;
+        }
         int tmp = arr[i];
         arr[i] = arr[j];
         arr[j] = tmp;
     }
 
-    public static void quickSort(int[] arr, int low, int high) {
-        int p, i, j;
-        if (low >= high) {
+    public static void quickSort(int[] arr, int l, int r) {
+        if (l >= r) {
             return;
         }
-        //p就是基准数,这里就是每个数组的第一个
-        p = arr[low];
-        i = low;
-        j = high;
+        int p = arr[l], i = l, j = r;
         while (i < j) {
-            //右边当发现小于p的值时停止循环
             while (arr[j] >= p && i < j) {
                 j--;
             }
-            //这里一定是右边开始，上下这两个循环不能调换（下面有解析，可以先想想）
-            //左边当发现大于p的值时停止循环
             while (arr[i] <= p && i < j) {
                 i++;
             }
             swap(arr, i, j);
         }
-        arr[low] = arr[i];//这里的arr[i]一定是停小于p的，经过i、j交换后i处的值一定是小于p的(j先走)
-        arr[i] = p;
-        quickSort(arr, low, j - 1);  //对左边快排
-        quickSort(arr, j + 1, high); //对右边快排
-
+        swap(arr, l, i);
+        quickSort(arr, l, j - 1);
+        quickSort(arr, j + 1, r);
     }
 }
